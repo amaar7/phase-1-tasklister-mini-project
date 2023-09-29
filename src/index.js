6532af3a8da14f6650bb6c623581c4f99de2f05a
amaar <amaar.ahmednur@student.moringaschool.com>
2023-09-29 17:21:38 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById("new-task-description").value;
+    const taskItem = document.createElement("li");
+    taskItem.innerText = taskDescription;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => {
+      taskItem.remove();
+    });
+
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+
+    taskForm.reset(); 
+  });
 });
